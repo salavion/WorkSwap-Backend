5f012af5a68d07fda0834e11877b95564a4a3ae2
kirya343 <kkodolov40@gmail.com>
2026-08-15 15:12:05 +0300

    убрали кор

---
+++ shared/src/main/java/org/workswap/shared/config/ApiConfig.java
@@ -0,0 +1,15 @@
+package org.workswap.shared.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "api")
+@Getter @Setter
+public class ApiConfig {
+
+    private String url;
+}

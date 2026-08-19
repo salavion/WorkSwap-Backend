6b227cfff99884e4aa219c660883c21360849743
kirya343 <kkodolov40@gmail.com>
2026-08-19 21:51:56 +0300

    фикс при переходе на новую версию

--- apps/RestAPI/src/main/java/org/workswap/WorkSwapApiApplication.java
+++ apps/RestAPI/src/main/java/org/workswap/WorkSwapApiApplication.java
@@ -2,7 +2,7 @@ package org.workswap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.scheduling.annotation.EnableScheduling;

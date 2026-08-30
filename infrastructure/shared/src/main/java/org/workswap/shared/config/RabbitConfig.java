d114ec4c27a778582cc7d2a91cb9a907b8b8ae31
kirya343 <kkodolov40@gmail.com>
2026-08-30 21:07:50 +0300

    updated spring version

--- infrastructure/shared/src/main/java/org/workswap/shared/config/RabbitConfig.java
+++ infrastructure/shared/src/main/java/org/workswap/shared/config/RabbitConfig.java
@@ -1,11 +1,13 @@
 package org.workswap.shared.config;
 
+import org.springframework.amqp.client.config.EnableAmqp;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAmqp
 public class RabbitConfig {
 
     @Bean
